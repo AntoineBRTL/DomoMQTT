9c5723270b0f7e3efdc3d5f83314f02dbd7f61bd
Ilona <ilolab15@gmail.com>
2023-05-09 11:34:48 +0200

    + Sript de subscription
    + Script avec tout les topics en durs

---
+++ Subscription/Sub.py
@@ -0,0 +1,38 @@
+import paho.mqtt.client as mqtt_client
+from Subscription.Topics import topics
+
+# Server
+MQTT_BROKER = "mqtt.gw.wlan"
+MQTT_PORT   = 1883
+KEEP_ALIVE  = 45
+
+# Appelée lors de logs
+def onLog(client, userdata, level, buffer):
+    print("Log: ${buffer}")
+
+# Appelée lors de la connection d'un client
+def onConnection(client, userdata, flags, rc):
+    print("New connection")
+
+# Appelée lorsque qu'un message est reçu
+def onMessage(client, userdata, message):
+    print(message.topic, message.payload)
+
+# Main 
+def main():
+    client = mqtt_client.Client(client_id="Raspberry")
+
+    client.on_message = onMessage
+    client.on_connect = onConnection
+    client.on_log     = onLog
+
+    client.username_pw_set(username="Rasp", password="RaspDomo")
+    client.connect(host=MQTT_BROKER, port=MQTT_PORT, keepalive=KEEP_ALIVE)
+
+    # Sub to all topics
+    for topic in topics:
+        client.subscribe(topic)
+
+    client.loop_forever()
+
+main()
---
+++ Subscription/Topics.py
@@ -0,0 +1,9 @@
+# Tout les topics sont dans ce fichier
+
+topics = [
+    "Maison/Test",
+
+    "Maison/Temperature/Interieur",
+    "Maison/Temperature/Exterieur",
+    "Maison/Ventilateur/Vitesse"
+]
